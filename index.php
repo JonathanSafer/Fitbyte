7af42bd2d5c2305062e40d71e9f8bff146dbbd36
Jonathan Safer <jonathansafer2@gmail.com>
2020-12-02 15:33:10 -0500

    added css and remade front page

--- index.php
+++ index.php
@@ -1,46 +1,37 @@
 <?php include 'init.php'; 
     include 'actions.php';
-    if (isset($_POST['total']) && isset($_POST['name']) && isset($_POST['exercise'])) {
-        $result = total($_POST['exercise'], $_POST['name']);
+    if (isset($_POST['log']) && isset($_POST['username']) && isset($_POST['password']) && isset($_POST['exercise']) && isset($_POST['quantity'])) {
+        $result = newEntry($_POST['username'], $_POST['exercise'], $_POST['quantity']);
     }
-    if (isset($_POST['log']) && isset($_POST['name']) && isset($_POST['exercise']) && isset($_POST['quantity'])) {
-        $result = newEntry($_POST['name'], $_POST['exercise'], $_POST['quantity']);
-    }
-
 ?>
 
 <!DOCTYPE html>
 <meta charset="utf-8">
 <title>FitByte</title>
+<head>
+    <link rel="stylesheet" href="fitbyte.css">
+</head>
 <body>
+    <h1> Welcome to FitByte </h1>
+    <input type="button" value="Login/Signup" style="float: right;">
+
+    <h2 class=center>Quick Add:</h2>
 
 
-    <?php if (isset($result)) { ?>
-        <h1> Result: <?php echo $result ?></h1>
-    <?php } ?>
-    <form action="/index.php" method="post">
-        Name:<br>
-        <select name="name">
-            <?php
-            $query = "SELECT * FROM people";
-            $result = mysqli_query($GLOBALS['conn'], $query) or die("Error at $query");
-            while($person = mysqli_fetch_assoc($result)){
-                $name = $person['name'];
-            ?>
-                <option name="name" value="<?php echo $name;?>"><?php echo $name ?></option>
-                <?php
-            }
-            ?>
-        </select><br>
+    <form class=center action="index.php" method="post">
+        Username:<br>
+        <input type="text" name="username"><br>
+        Password:<br>
+        <input type="text" name="password"><br>
         Exercise:<br>
         <input type="text" name="exercise"><br>
-        <button type="submit" value="total" name="total">Submit</button>
+        Quantity: (For a plank enter # of seconds)<br>
+        <input type="text" name="quantity"><br>
+        <button type="submit" value="log" name="log">Log Data</button>
     </form>
-
-    <form action="/index.php" method="post">
-        <button type="submit" value="input" name="input">Log Data</button>
-    </form>
-
+    <?php if (isset($result)) { ?>
+        <h3 class=center><?php echo $result ?></h3>
+    <?php } ?>
 
     <?php if (isset($_POST['input'])) { ?>
         <form action="/index.php" method="post">
