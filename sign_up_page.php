5e00261569fa4388f76b63995ca254be1061cc0f
robertquartey7 <37972474+robertquartey7@users.noreply.github.com>
2020-12-03 16:50:23 -0500

    Update sign_up_page.php

--- sign_up_page.php
+++ sign_up_page.php
@@ -3,23 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href="fitbyte.css">
     <title>fitbyte</title>
 </head>
 <body>
-    
-<div class="center" >
-                    <form action="login.php">
+<h1 > Welcome to FitByte </h1>
+<div class="center1" >
+                    
+     <form action="login.php">
+         <input type="text" placeholder="First Name" name="first_name" id="fname" >
+        <input type="text" placeholder="Last Name" name="last_name" id="lname" ><br />
+        <input type="text" name="username" id="username" placeholder="username">
+        <input type="text" placeholder="Email" name="email" id="mail" > <br />
+         <input type="password" placeholder="Password" name="account_password" id="password" >
+        <input type="password" placeholder="Confirm Password" name="confirm_password" id="cpassword" ><br />
+         <input type="submit" value="Create Account" class="sumbit">
                         
-                        <input type="text" placeholder="First Name" name="first_name" id="fname" >
-                        <input type="text" placeholder="Last Name" name="last_name" id="lname" ><br />
-                        <input type="text" name="username" id="username" placeholder="username">
-                        <input type="text" placeholder="Email" name="email" id="mail" > <br />
-                        <input type="password" placeholder="Password" name="account_password" id="password" >
-                        <input type="password" placeholder="Confirm Password" name="confirm_password" id="cpassword" ><br />
-                        <input type="submit" value="Create Account" class="sumbit">
-                        
-                    </form>
+      </form>
 
 </div>
 
