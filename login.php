08b9d8c85b198dc4abbf2769853635ea8fd3f4d7
robertquartey7 <37972474+robertquartey7@users.noreply.github.com>
2020-12-03 16:12:46 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="fitbyte.css">
+    <title>fitbyte</title>
+</head>
+<body>
+<div class="center">
+        <form action="#" method="POST" class="">
+            Email: <input type="email" name="email"> <br>
+            Password: <input type="password" name="password"> <br>
+            <input type="submit" value="" class="submit">
+        </form>
+</div>
+</body>
+</html>
