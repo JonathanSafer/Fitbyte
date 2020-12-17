c7a84011bfe4226e64143810c2d2d83d2cd80ecb
robertquartey7 <37972474+robertquartey7@users.noreply.github.com>
2020-12-17 16:43:44 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,8 +2,8 @@
 <footer>
     <div class="footer">
     <div class="footer-content">
-        <a href="about">about</a>
-        <a href="contact">contact</a>
+        <a href="https://github.com/JonathanSafer/Fitbyte/blob/main/README.md">about</a>
+        <a href="#">contact</a>
     </div>
     </div>
 </footer>
